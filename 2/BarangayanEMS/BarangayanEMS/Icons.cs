236bcda0f922764135bbf9d2b87cf291c158b7a1
Raf <ralphquiaot27@gmail.com>
2025-12-08 16:25:47 +0800

    Add files via upload

---
+++ 2/BarangayanEMS/BarangayanEMS/Icons.cs
@@ -0,0 +1,11 @@
+public static class Icons
+{
+    public static string seal; // Add this field to match usage in LoginForm.Designer.cs
+    public static string QR;
+    public static string Lock;
+    public static string Digital;
+    public static string Mail;
+    public static string Shield;
+    public static string Check;
+    public static string User;
+}
